d57d059de939a77456cb4c50d1581d6dc7af1bce
Joseph Gordon <jegordon42@gmail.com>
2019-02-19 17:56:14 -0500

    second commit

---
+++ DuckLab/Controllers/CompaniesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DuckLab.Models;
+using EntityState = System.Data.Entity.EntityState;
+
+namespace DuckLab.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private ducklabdbEntities db = new ducklabdbEntities();
+
+        // GET: Companies
+        public ActionResult Index()
+        {
+            return View(db.Companies.Take(10).ToList());
+        }
+
+        // GET: Companies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "companyID,companyName,companySymbol")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Companies.Add(company);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(company);
+        }
+
+        // GET: Companies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "companyID,companyName,companySymbol")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(company).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Company company = db.Companies.Find(id);
+            db.Companies.Remove(company);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- DuckLab/Controllers/HomeController.cs
+++ DuckLab/Controllers/HomeController.cs
@@ -3,14 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using DuckLab.Models;
+using System.Data.Entity;
 
 namespace DuckLab.Controllers
 {
     public class HomeController : Controller
     {
+        private ducklabdbEntities db = new ducklabdbEntities();
+        public static string baseURL = "https://api.iextrading.com/1.0/";
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Companies.Take(10).ToList());
+        }
+
+        public RedirectToRouteResult GetStocks()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseURL + "ref-data/symbols");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string strResponse = response.Content.ReadAsStringAsync().Result;
+                while(strResponse.IndexOf("symbol") != -1)
+                {
+                    string symbol = "";
+                    int index = strResponse.IndexOf("symbol") + 9;
+                    for (; strResponse[index] != '"'; index++)
+                    {
+                        symbol = symbol + strResponse[index];
+                    }     
+                    
+                    string companyName = "";
+                    index = strResponse.IndexOf("name") + 7;
+                    for (; strResponse[index] != '"'; index++)
+                    {
+                        companyName = companyName + strResponse[index];
+                    }
+
+                    Company newCompany = new Company();
+                    newCompany.companyName = companyName;
+                    newCompany.companySymbol = symbol;
+                    db.Companies.Add(newCompany);
+                    db.Entry(newCompany).State = EntityState.Added;
+
+                    strResponse = strResponse.Substring(index);
+                }
+                db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            client.Dispose();
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
